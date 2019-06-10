28c11ffd8f7cafede905781927dd2a808f3ac2e4
GrigoryCode <grigorycode@gmail.com>
2019-06-10 13:00:36 +0300

    Add new and create actions with view files

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,18 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(play_params)
+  end
+
+  private
+
+  def play_params
+    params.require(:item).permit(:title, :description)
+  end
 end
