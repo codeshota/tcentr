08f5976eb148899a0573fec56da09012f2c29068
GrigoryCode <grigorycode@gmail.com>
2019-06-15 14:15:38 +0300

    Items title validation and items controller correction

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -16,14 +16,13 @@ class ItemsController < ApplicationController
     @item = Item.new(item_params)
 
     if @item.save
-      redirect_to root_path
+      redirect_to items_path
     else
       render 'new'
     end
   end
 
   def edit
-
   end
 
   def update
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,3 @@
 class Item < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 5 }
 end
