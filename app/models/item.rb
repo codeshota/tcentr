f3f2424e02db553fa4268df74c7caa440fb8074d
GrigoryCode <grigorycode@gmail.com>
2019-06-16 02:22:39 +0300

    Images for catalog page

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,5 +7,5 @@ class Item < ApplicationRecord
   validates :price, presence: true
 
   has_attached_file :item_img, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+  validates_attachment_content_type :item_img, content_type: /\Aimage\/.*\z/
 end
